3fa6b314425405f7e53f219877fec3a95d2ccdb4
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2025-03-23 16:33:46 -0300

    the amount propertie can not be ushort

--- Core/Models/Entities/Positions.cs
+++ Core/Models/Entities/Positions.cs
@@ -4,12 +4,12 @@ public class Positions : BaseEntity
 {
     public int ClientId { get; private set; }
     public int AssetId { get; private set; }
-    public ushort Amount { get; private set; }
+    public short Amount { get; private set; }
     public DateOnly Date { get; private set; }
     public DateTime CreatedAt { get; private set; }
     public DateTime UpdatedAt { get; private set; }
     
-    public Positions(int clientId, int assetId, ushort amount, DateOnly date)
+    public Positions(int clientId, int assetId, short amount, DateOnly date)
     {
         ClientId = clientId;
         AssetId = assetId;
@@ -17,7 +17,7 @@ public class Positions : BaseEntity
         Date = date;
     }
 
-    public Positions(int clientId, int assetId, ushort amount, DateOnly date, DateTime createdAt, DateTime updatedAt)
+    public Positions(int clientId, int assetId, short amount, DateOnly date, DateTime createdAt, DateTime updatedAt)
     {
         ClientId = clientId;
         AssetId = assetId;
@@ -26,4 +26,6 @@ public class Positions : BaseEntity
         CreatedAt = createdAt;
         UpdatedAt = updatedAt;
     }
+
+    public string ToString() => $"ClientId: {ClientId}, AssetId: {AssetId}, Amount: {Amount}, Date: {Date}";
 }
--- Core/Models/Events/OperationCreated.cs
+++ Core/Models/Events/OperationCreated.cs
@@ -6,11 +6,11 @@ public struct OperationCreated
 {
     public int ClientId { get; private set; }
     public int AssetId { get; private set; }
-    public ushort Amount { get; private set; }
+    public short Amount { get; private set; }
     
     public OperationType OperationType { get; private set; }
     public DateTimeOffset Moment { get; private set; }
     
-    public OperationCreated(int clientId, int assetId, ushort amount, OperationType operationType) =>
+    public OperationCreated(int clientId, int assetId, short amount, OperationType operationType) =>
         (ClientId, AssetId, Amount, OperationType, Moment) = (clientId, assetId, amount, operationType, DateTimeOffset.Now);
 }
