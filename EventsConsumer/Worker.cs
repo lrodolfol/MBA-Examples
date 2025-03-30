ea0c7090e7965a4d13b95bd184f06a27ddaaf232
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2025-03-30 09:08:55 -0300

    Services like properti class

--- EventsConsumer/Worker.cs
+++ EventsConsumer/Worker.cs
@@ -13,16 +13,17 @@ namespace EventsConsumer;
 
 public class Worker : BackgroundService
 {
+    private ushort _consumerCount = 5;
     private readonly ILogger<Worker> _logger;
-
     private ConnectionFactory _factory;
     private IConnection _connection;
     private IChannel _channel;
-    
-    IServiceProvider _serviceProvider;
 
     private readonly MyConfigurations.RabbitMqConfiguration _rabbitMqConfiguration =
         MyConfigurations.RabbitMqEnvironment;
+
+    private readonly OperationsService _operationsServices;
+    private readonly PositionsService _positionsService;
 
     public Worker(ILogger<Worker> logger, IServiceProvider serviceProvider)
     {
@@ -34,11 +35,14 @@ public class Worker : BackgroundService
             Port = Convert.ToInt16(_rabbitMqConfiguration.Port ?? "5672"),
             VirtualHost = _rabbitMqConfiguration.VirtualHost ?? "/",
         };
+        
         _connection = _factory.CreateConnectionAsync().Result;
         _channel = _connection.CreateChannelAsync().Result;
-        _channel.BasicQosAsync(0, 1, false);
+        _channel.BasicQosAsync(0, _consumerCount, false);
         _logger = logger;
-        _serviceProvider = serviceProvider;
+
+        _positionsService = serviceProvider.GetRequiredService<PositionsService>();
+        _operationsServices = serviceProvider.GetRequiredService<OperationsService>();
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -56,45 +60,52 @@ public class Worker : BackgroundService
 
     private async Task OnMessage(object sender, BasicDeliverEventArgs eventArgs)
     {
-        // _ = Task.Run(async () =>
-        // {
-            var message = eventArgs.Body.ToArray().ToUTF8String();
-            var operationCreated = message.Deserialize<OperationCreated>();
+        await Task.Run(() => ProcessMessageAsync(eventArgs));
+    }
+
+    private async Task ProcessMessageAsync(BasicDeliverEventArgs eventArgs)
+    {
+        var message = eventArgs.Body.ToArray().ToUTF8String();
+        var operationCreated = message.Deserialize<OperationCreated>();
             
-            _logger.LogInformation("Receives from routinkey. Message: {0}", message);
-
-            try
-            {
-                //colocar UnitOfWork para todos processos.
-
-                var operationsServices = _serviceProvider.GetRequiredService<OperationsService>(); //deixar como propriedade da classe
-                await operationsServices.ProcessOperationReceivedAsync(operationCreated);
-
-                var positionsService = _serviceProvider.GetRequiredService<PositionsService>(); //deixar como propriedade da classe
-                var amoutConverted = operationCreated.OperationType == OperationType.INPUT
-                    ? operationCreated.Amount : operationCreated.Amount * -1;
+        //_logger.LogInformation("Receives from routinkey. Message: {0}", message);
+            
+        try
+        {
+            //usar um serviço externo para validar se data é feriado
+            // if (operationCreated.Moment.DayOfWeek == DayOfWeek.Sunday || operationCreated.Moment.DayOfWeek == DayOfWeek.Saturday)
+            //     throw new InvalidBusinessDayException("The position date is not a business day for position -> " + operationCreated.ToString());
                 
-                await positionsService.UpInsertPositionAsync(
-                    new Positions(
-                        operationCreated.ClientId, 
-                        operationCreated.AssetId, 
-                        (short)amoutConverted,
-                        DateOnly.FromDateTime(operationCreated.Moment.DateTime)
-                        )
-                    );
+            //colocar UnitOfWork para todos processos de escrita na base.
+            
+            await _operationsServices.ProcessOperationReceivedAsync(operationCreated);
+
+            
+            var amoutConverted = operationCreated.OperationType == OperationType.INPUT
+                ? operationCreated.Amount : operationCreated.Amount * -1;
+            
+            await _positionsService.UpInsertPositionAsync(
+                new Positions(
+                    operationCreated.ClientId, 
+                    operationCreated.AssetId, 
+                    (short)amoutConverted,
+                    DateOnly.FromDateTime(operationCreated.Moment.DateTime)
+                )
+            );
                 
-                await _channel.BasicAckAsync(eventArgs.DeliveryTag, false);
-            }
-            catch (Exception e) when(e is InvalidBusinessDayException || e is NegativeAmountException)
-            {
-                _logger.LogError("Error on process operation. Details: {0}", e.Message);
-                await _channel.BasicNackAsync(eventArgs.DeliveryTag, multiple: false, requeue: false);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError("Error on process operation. Details: {0}", e.Message);
-                await _channel.BasicNackAsync(eventArgs.DeliveryTag, multiple: false, requeue: true);
-            }
-        //});
+            await _channel.BasicAckAsync(eventArgs.DeliveryTag, false);
+        }
+        catch (Exception e) when(e is InvalidBusinessDayException 
+                                 || e is NegativeAmountException 
+                                 || e is InsufficientBalanceException)
+        {
+            _logger.LogError("Error on process operation. Details: {0}", e.Message);
+            await _channel.BasicNackAsync(eventArgs.DeliveryTag, multiple: false, requeue: false);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError("Error on process operation. Details: {0}", e.Message);
+            await _channel.BasicNackAsync(eventArgs.DeliveryTag, multiple: false, requeue: true);
+        }
     }
 }
